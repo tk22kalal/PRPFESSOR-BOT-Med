df15fcad68552d26dbe192c1603f13b2ef05ec36
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-03-15 22:29:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,9 +21,9 @@ def is_enabled(value, default):
 PORT = environ.get("PORT", "8080")
 WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+API_ID = int(environ['24058425'])
+API_HASH = environ['694b063e55c24287a3d30aed90191373']
+BOT_TOKEN = environ['5811522115:AAGDhxfqrugvv3dtvgVIveilAjX5gca21Fo']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -42,7 +42,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tk22kalal:EdFdkZtqeK7e8kdo@cluster0.z6sfzj9.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
